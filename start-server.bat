b0b61be5b3f66a7073b3ab17f5a4874cde8ec585
KatsuneCurry <Danielcong4@gmail.com>
2026-01-21 10:26:09 +0000

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "%~dp0"
+cd /d %~dp0
 echo Checking dependencies
 if not exist "node_modules" (
     echo Installing dependencies
@@ -8,5 +8,6 @@ if not exist "node_modules" (
 echo Starting server
 node server.js
 pause
+
 
 
